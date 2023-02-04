d1bc5aceda8b60f83c7d218c028044dda1945511
Angel Romero <mrangelrcd@gmail.com>
2023-02-04 16:23:44 +0100

    crawl single page with error handiling

--- crawl.js
+++ crawl.js
@@ -1,6 +1,31 @@
 const { log } = require('console')
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentURL){
+  console.log(`actively crawling ${currentURL}`)
+
+  try {
+    const resp = await fetch(currentURL)
+
+    if(resp.status > 399){
+      console.log(`error in fetch with status code: ${resp.status} on page ${currentURL}`);
+      return
+    }
+
+    const contentType = resp.headers.get('content-type')
+    if (!contentType.includes("text/html")){
+      console.log(`non html response: content type ${contentType} on page ${currentURL}`);
+      return
+    }
+
+    console.log(await resp.text())
+  } catch (err) {
+    console.log(`error in fetch: ${err.message}`);
+  }
+
+
+}
+
 function getURLsFromHTML(htmlBody, baseURL){
   const urls=[]
   const dom = new JSDOM(htmlBody)
@@ -42,6 +67,7 @@ function normalizeURL(urlString){
 
 
 module.exports = {
+  crawlPage,
   normalizeURL,
   getURLsFromHTML
 }
